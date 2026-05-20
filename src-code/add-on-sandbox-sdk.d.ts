cfe209bbc9b218ecd68460d05e6fccde4ae6b52b
Justin Taylor <justin@hyperbrew.co>
2026-05-20 07:47:00 -0700

    fix type imports

--- src-code/add-on-sandbox-sdk.d.ts
+++ src-code/add-on-sandbox-sdk.d.ts
@@ -1,9 +1,9 @@
 declare module "add-on-sdk-document-sandbox" {
   import { AddOnDocumentSandboxSdkTypes } from "@adobe/ccweb-add-on-sdk-types";
   export default AddOnDocumentSandboxSdkTypes.default;
-  export * from "@adobe/ccweb-add-on-sdk-types/sandbox/add-on-sdk-document-sandbox";
+  export * from "@adobe/ccweb-add-on-sdk-types/add-on-sdk-document-sandbox";
 }
 
 declare module "express-document-sdk" {
-  export * from "@adobe/ccweb-add-on-sdk-types/sandbox/express-document-sdk";
+  export * from "@adobe/ccweb-add-on-sdk-types/express-document-sdk";
 }
