76022a16d99ec875c83f1157b102ac55a83bb50e
Justin Taylor <justin@hyperbrew.co>
2026-06-09 14:00:24 -0700

    using ws library

--- scripts/custom-server.ts
+++ scripts/custom-server.ts
@@ -2,7 +2,12 @@ import fs from "fs";
 import path from "path";
 import https from "https";
 import mkcert from "@mkcert/node";
-import handler from "serve-handler";
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
+import { WebSocketServer } from "ws";
+
+import { config } from "../express.config";
 
 const PORT = 5242;
 
@@ -14,7 +19,7 @@ const addOnResponse = {
       versionString: "0.0.20",
       supportedLanguages: ["en-US"],
       supportedApps: ["Express"],
-      downloadUrl: "https://localhost:5241/bolt-express/manifest.json",
+      downloadUrl: `https://localhost:${PORT}/${config.manifest.testId}/manifest.json`,
       addon: {
         localizedMetadata: {
           name: "Bolt Express",
@@ -53,17 +58,56 @@ async function main() {
   mkcert.installSync();
 
   console.log(`Starting Custom Server on https://localhost:${PORT}`);
-  https
-    .createServer(
-      {
-        key: fs.readFileSync("./scripts/key.pem"),
-        cert: fs.readFileSync("./scripts/cert.pem"),
-      },
-      (req, res) => {
-        res.writeHead(200);
-        res.end(JSON.stringify(addOnResponse));
-      },
-    )
-    .listen(PORT);
+
+  const app = express();
+  app.use(cors());
+  app.get("/", (req, res) => {
+    res.send(addOnResponse);
+  });
+  app.use(`/${config.manifest.testId}`, express.static("dist"));
+
+  const server = https.createServer(
+    {
+      key: fs.readFileSync("./scripts/key.pem"),
+      cert: fs.readFileSync("./scripts/cert.pem"),
+      requestCert: false,
+      rejectUnauthorized: false,
+    },
+    app,
+    //   (req, res) => {
+    //     res.writeHead(200);
+    //     res.end(JSON.stringify(addOnResponse));
+    //   },
+  );
+  // socket io route
+  //   const io = new Server(server);
+
+  //   io.on("connection", (client) => {
+  //     console.log("WS Connect");
+  //     client.on("event", (data) => {
+  //       console.log("WS Event");
+  //       /* … */
+  //     });
+  //     client.on("disconnect", () => {
+  //       console.log("WS Disconnect");
+  //       /* … */
+  //     });
+  //   });
+
+  const wss = new WebSocketServer({ server });
+
+  wss.on("connection", function connection(ws) {
+    ws.on("error", console.error);
+
+    ws.on("message", function message(data) {
+      console.log("received: %s", data);
+    });
+
+    ws.send("something");
+  });
+
+  server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+  });
 }
 main();
